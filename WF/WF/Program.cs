2cdcac36e58c268a53f5393ac3b6ae60d6c3d7ff
zhangxuelei <1016716602@qq.com>
2020-02-02 13:37:50 +0800

    1

---
+++ WF/WF/Program.cs
@@ -0,0 +1,31 @@
+﻿using Gtk;
+using System;
+
+namespace WF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init();//初始化
+
+            var win = new Window("Hello World");//新建窗体，标题是Hello World
+            win.WindowPosition = WindowPosition.Center;
+            win.DefaultHeight = 768;
+            win.DefaultWidth = 1024;
+
+            var lable = new Label("亮度增益加测");//新建标签
+            lable.Valign = Align.Start;
+            lable.Halign = Align.Center;
+            lable.Ypad = 20;
+            
+
+            
+
+            win.Add(lable);//将标签加入到窗体
+            win.ShowAll();//显示窗体
+            Application.Run();//运行窗体 
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
